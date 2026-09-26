f480dd607150f5f14da4b3b871b8817550671a84
ney73 <ezbyheaven@gmail.com>
2026-09-26 13:10:13 +0200

    feat: ajout du module scan manga

--- scripts/verify-repository.mjs
+++ scripts/verify-repository.mjs
@@ -11,7 +11,7 @@ const expectedHandlers = {
   "internet-archive-text": ["searchResults", "extractDetails", "extractChapters", "extractText"],
   "novelneko-wn": ["searchResults", "extractDetails", "extractChapters", "extractText"],
   "novelneko-ln": ["searchResults", "extractDetails", "extractResources"],
-  "j-garden": ["searchResults", "extractDetails", "extractChapters", "extractResources"],
+  "scan-manga": ["searchResults", "extractDetails", "extractChapters", "extractImages"],
   "anime-sama": ["searchResults", "extractDetails", "extractChapters", "extractImages"],
   lnori: ["searchResults", "extractDetails", "extractChapters", "extractText"],
   witchculttranslation: ["searchResults", "extractDetails", "extractChapters", "extractText"],
